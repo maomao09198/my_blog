ee2b63d16d3e77a679029cc52bf6f432f1bc8ed2
maomao09198 <rojakorim42@gmail.com>
2025-03-15 17:35:59 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const toggleButton = document.getElementById("toggle-button");
+
+const logo = document.querySelector(".logo img")
+
+console.log(localStorage.getItem("theme"));
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleButton.classList.add("dark");
+    logo.src = "./assets/logodark.png";
+
+}
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    toggleButton.classList.toggle("dark");
+
+    // Save theme preference
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        logo.src = "./assets/logodark.png";
+    } else {
+        localStorage.setItem("theme", "light");
+        logo.src = "./assets/logowhite.png"
+    }
+});
