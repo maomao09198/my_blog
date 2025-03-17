44aaf5e53769686d842485f4fdc42f40d99f91fb
maomao09198 <rojakorim42@gmail.com>
2025-03-17 17:21:37 +0600

    Fourth

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
-const toggleButton = document.getElementById("toggle-button");
+// const toggleButton = document.getElementById("toggle-button");
 
-const logo = document.querySelector(".logo img")
+// const logo = document.querySelector(".logo img")
 
-console.log(localStorage.getItem("theme"));
+// console.log(localStorage.getItem("theme"));
 
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark-mode");
-    toggleButton.classList.add("dark");
-    logo.src = "./assets/logodark.png";
+// if (localStorage.getItem("theme") === "dark") {
+//     document.body.classList.add("dark-mode");
+//     toggleButton.classList.add("dark");
+//     logo.src = "./assets/logodark.png";
 
-}
-toggleButton.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    toggleButton.classList.toggle("dark");
+// }
+// toggleButton.addEventListener("click", () => {
+//     document.body.classList.toggle("dark-mode");
+//     toggleButton.classList.toggle("dark");
 
-    // Save theme preference
-    if (document.body.classList.contains("dark-mode")) {
-        localStorage.setItem("theme", "dark");
-        logo.src = "./assets/logodark.png";
-    } else {
-        localStorage.setItem("theme", "light");
-        logo.src = "./assets/logowhite.png"
-    }
-});
+//     // Save theme preference
+//     if (document.body.classList.contains("dark-mode")) {
+//         localStorage.setItem("theme", "dark");
+//         logo.src = "./assets/logodark.png";
+//     } else {
+//         localStorage.setItem("theme", "light");
+//         logo.src = "./assets/logowhite.png"
+//     }
+// });
+
