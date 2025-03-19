80817dd1e882111b92fb3d53f5127b1a1afe8c5f
maomao09198 <rojakorim42@gmail.com>
2025-03-19 16:55:17 +0600

    Seventh Commit

--- js/script.js
+++ js/script.js
@@ -1,33 +1,41 @@
-// const toggleButton = document.getElementById("toggle-button");
+const toggleButton = document.getElementById("toggle-button");
 
-// const logo = document.querySelector(".logo img")
+const logo = document.querySelector(".logo img")
+const menuItem = document.querySelector(".menu-item a");
 
-// console.log(localStorage.getItem("theme"));
+console.log(localStorage.getItem("theme"));
 
-// if (localStorage.getItem("theme") === "dark") {
-//     document.body.classList.add("dark-mode");
-//     toggleButton.classList.add("dark");
-//     logo.src = "./assets/logodark.png";
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+    toggleButton.classList.add("dark");
+    logo.src = "./assets/logodark.png";
 
-// }
-// toggleButton.addEventListener("click", () => {
-//     document.body.classList.toggle("dark-mode");
-//     toggleButton.classList.toggle("dark");
 
-//     // Save theme preference
-//     if (document.body.classList.contains("dark-mode")) {
-//         localStorage.setItem("theme", "dark");
-//         logo.src = "./assets/logodark.png";
-//     } else {
-//         localStorage.setItem("theme", "light");
-//         logo.src = "./assets/logowhite.png"
-//     }
-// });
+}
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    toggleButton.classList.toggle("dark");
 
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+    // Save theme preference
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        logo.src = "./assets/logodark.png";
+        menuItem.color ="white";
+    } else {
+        localStorage.setItem("theme", "light");
+        logo.src = "./assets/logowhite.png";
+        menuItem.color ="#202020";
+
+    }
+});
+
+const menuIcon = document.getElementById("menuIcon");
+const menu = document.getElementById("menu");
+menuIcon.addEventListener("click", ()=>{
+    if(menu.style.display === "block"){
+        menu.style.display = "none"
+    }
+    else {
+        menu.style.display = "block"
+    }
+});
